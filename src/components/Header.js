5c21430100ea14960106a684bfe47c10d11cba26
vins <106815864+badlaaag@users.noreply.github.com>
2023-05-09 02:05:22 +0800

    added image
    added image

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
                     <div className="header-upper-links d-flex align-items-center justify-content-between">
                     <div>
                         <Link className='d-flex align-items-center gap-10 text-white'>
-                        {/*  <img src="images/compare.svg" alt="" /> */}
+                        <img src="images/compare.png" alt="" />
                             <p className='mb-0'>
                                 Compare <br/> Products
                             </p>
@@ -51,7 +51,7 @@ const Header = () => {
                     </div>
                     <div>
                         <Link className='d-flex align-items-center gap-10 text-white'>
-                          {/*<img src="images/wishlist.svg" alt="" />*/} 
+                        <img src="images/wishlist.svg" alt="" />
                             <p className='mb-0'>
                                 Favourite <br/> Wishlist
                             </p>
@@ -59,7 +59,7 @@ const Header = () => {
                     </div>
                     <div>
                         <Link className='d-flex align-items-center gap-10 text-white'>
-                       {/*   <img src="images/user.svg" alt="" />*/} 
+                        <img src="images/user.svg" alt="" />
                             <p className='mb-0'>
                             Log in <br/>My Account
                             </p>
