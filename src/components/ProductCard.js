ae091379e6ec443bd235a7ccdc550b60299a6aa5
gerbeace <128573471+gerbeace@users.noreply.github.com>
2023-05-09 18:25:49 +0800

    product card
    nilagyan ko ng hover na mag aappear yung view compare and add to cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
   return (
     <div className="col-3">
-        <div className="product-card">
+        <div className="product-card position-relative">
             <div className="product-image">
                 <img src="images/watch.jpg" alt="product image" />
             </div>
@@ -14,9 +15,24 @@ const ProductCard = () => {
                 <ReactStars
                     count={5}   
                     size={24}
+                    value="3"
+                    edit={false}
                     activeColor="#ffd700"
                 />
                 <p className="price">P1000.00</p>
+            </div>
+            <div className="action-bar position-absolute">
+                <div className="d-flex flex-column gap-15">
+                    <Link>
+                    <img src="images/prodcompare.svg" alt="compare" />
+                    </Link>
+                    <Link>
+                    <img src="images/view.svg" alt="view" />
+                    </Link>
+                    <Link>
+                    <img src="images/add-cart.svg" alt="addtocart" />
+                    </Link>
+                </div>
             </div>
         </div>
     </div>
