e07c01154c34b756821f5bfd013e1e1ad5c91cd0
gerbeace <128573471+gerbeace@users.noreply.github.com>
2023-05-13 12:42:10 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,79 +30,70 @@ const Header = () => {
                 </div>
             </div>
         </div>
-      </header>
-      <header className="header-upper py-3">
-        <div className="container-xxl">
-          <div className="row align-items-center">
-            <div className="col-2">
-              <h2>
-                <Link className="text-white">Dev Corner</Link>
-              </h2>
-            </div>
-            <div className="col-5">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control py-2"
-                  placeholder="Search Product Here..."
-                  aria-label="Search Product Here..."
-                  aria-describedby="basic-addon2"
-                />
-                <span className="input-group-text p-3" id="basic-addon2">
-                  <BsSearch className="fs-6" />
-                </span>
-              </div>
-            </div>
-            <div className="col-5">
-              <div className="header-upper-links d-flex align-items-center justify-content-between">
-                <div>
-                  <Link
-                    to="/compare-product"
-                    className="d-flex align-items-center gap-10 text-white"
-                  >
-                    <img src={compare} alt="compare" />
-                    <p className="mb-0">
-                      Compare <br /> Products
-                    </p>
-                  </Link>
+    </header>
+    <header className='header-upper py-3'>
+        <div className='container-xxl'>
+            <div className='row align-items-center'>
+                <div className='col-2'>
+                    <h1>
+                        <Link to='/' className='text-white fs-3'>Tech-O-Matic</Link>
+                    </h1>
                 </div>
-                <div>
-                  <Link
-                    to="/wishlist"
-                    className="d-flex align-items-center gap-10 text-white"
-                  >
-                    <img src={wishlist} alt="wishlist" />
-                    <p className="mb-0">
-                      Favourite <br /> wishlist
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link
-                    to="/login"
-                    className="d-flex align-items-center gap-10 text-white"
-                  >
-                    <img src={user} alt="user" />
-                    <p className="mb-0">
-                      Log in <br /> My Account
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link
-                    to="/cart"
-                    className="d-flex align-items-center gap-10 text-white"
-                  >
-                    <img src={cart} alt="cart" />
-                    <div className="d-flex flex-column gap-10">
-                      <span className="badge bg-white text-dark">0</span>
-                      <p className="mb-0">$ 500</p>
+                <div className='col-5'>
+                    <div className="input-group">
+                    <input
+              type="text"
+              className="form-control py-2"
+              placeholder="Search Products..."
+              aria-label="Search Products..."
+              aria-describedby="basic-addon2"
+              value={searchQuery} // Display the search query value in the input field
+              onChange={handleSearch}
+              onKeyPress={handleKeyPress}
+            />
+                        <span class="input-group-text p-3" id="basic-addon2">
+                           <BsSearch className='fs-6' />
+                        </span>
                     </div>
-                  </Link>
                 </div>
-              </div>
+                <div className='col-5'>
+                    <div className="header-upper-links d-flex align-items-center justify-content-between">
+                    <div>
+                        <Link to ='/compare-product' className='d-flex align-items-center gap-10 text-white'>
+                        <img src="images/compare.png" alt="" />
+                            <p className='mb-0'>
+                                Compare <br/> Products
+                            </p>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to='wishlist' className='d-flex align-items-center gap-10 text-white'>
+                        <img src="images/wishlist.svg" alt="" />
+                            <p className='mb-0'>
+                                Favourite <br/> Wishlist
+                            </p>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to='/login' className='d-flex align-items-center gap-10 text-white'>
+                        <img src="images/user.svg" alt="" />
+                            <p className='mb-0'>
+                            Log in <br/>My Account
+                            </p>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to ='/cart' className='d-flex align-items-center gap-10 text-white'>
+                        <img src="images/cart.png" alt="cart" />
+                            <div className='d-flex flex-column'>
+                                <span className='badge bg-white text-dark'>0</span>
+                                <p className='mb-0'>₱ 0.00</p>
+                            </div>
+                        </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
     </header>
     <header className='header-bottom py-3'>
